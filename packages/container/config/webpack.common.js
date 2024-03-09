118f6d4c255f5bf2bb04757ab04340ac65d1a575
Kilmer Luiz <kilmerluizale@gmail.com>
2024-03-09 17:11:28 -0300

    added dashboard.

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,25 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-module.exports = { 
+module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?jsx?$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
             plugins: ['@babel/plugin-transform-runtime'],
-          }          
-        }
-      }
-    ]
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
